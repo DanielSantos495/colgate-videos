b984b2c323522950a2fbcf5836250dd6f55bf4c5
Daniel Santos <dsantos0495@gmail.com>
2022-02-28 21:42:33 -0500

    First test

---
+++ config/index.js
@@ -0,0 +1,21 @@
+const devices = {
+  devices: [
+    {
+      name: 'sensor0',
+      driver: 'analogSensor',
+      pin: 7
+    },
+    {
+      name: 'sensor1',
+      driver: 'analogSensor',
+      pin: 29
+    },
+    {
+      name: 'sensor2',
+      driver: 'analogSensor',
+      pin: 31
+    }
+  ]
+}
+
+module.exports =  { devices }
---
+++ index.js
@@ -0,0 +1,24 @@
+const Cylon = require('cylon')
+const { devices } = require('./config/index')
+
+const robot = Cylon.robot({
+  connections: { raspi: { adaptor: 'raspi' } }, //Config para raspi
+  devices, //Dispositivos conectados a la raspberry pi (Sensores en este cas0)
+  work: function (my) {
+    // Aquí es donde podemos trabajar con el robot de cylon (Con los driver)
+
+    let analogValue = 0;
+
+    for (let i = 0; i < devices.length; i++) {
+      const sensorName = devices[i].name;
+      my.devices[sensorName].on('analogRead', () => {
+        analogValue = my.sensor.analogRead() // Devuelve el valor del sensor
+
+        console.log({sensorActive: analogValue})
+        console.log(my)
+      })
+    }
+  },
+})
+
+robot.start()
